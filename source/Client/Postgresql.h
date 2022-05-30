5a6531fe4b5db4c71d01f69f10adfa2502be9a59
Sriram <105226769+i-m-sri@users.noreply.github.com>
2022-05-30 09:55:51 +0530

    Add files via upload

---
+++ source/Client/Postgresql.h
@@ -0,0 +1,14 @@
+#pragma once
+#pragma warning(disable:4996)
+#include<iostream>
+#include"VariadicTable.h"
+#include"text_color.h"
+#include<thread>
+#include<winsock.h>
+#include<string>
+
+std::string get_value(std::string);
+void execute(std::string, int);
+void check_inbox(std::string,int);
+
+
