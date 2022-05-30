032f97e14b2f89c236d540591e882ae2ab472057
Sriram <105226769+i-m-sri@users.noreply.github.com>
2022-05-30 09:20:48 +0530

    Update Postgresql.h

--- source/Client/Postgresql.h
+++ source/Client/Postgresql.h
@@ -1,14 +1,11 @@
 #pragma once
 #pragma warning(disable:4996)
-#include<pqxx/pqxx>
 #include<iostream>
 #include"VariadicTable.h"
 #include"text_color.h"
 #include<thread>
 #include<winsock.h>
 #include<stdio.h>
-
-using namespace pqxx;
 
 std::string get_value(std::string);
 void execute(std::string, int);
